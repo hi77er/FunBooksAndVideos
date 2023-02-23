9a610c97d6bc29ba4121f12cb5340548a11dd7cd
hi77er <kkrastev@live.com>
2023-02-23 12:42:45 +0200

    Removed commented lines from Automapper Profile.

--- FunBooksAndVideos.WebApi/Mapper/EntityDTOProfile.cs
+++ FunBooksAndVideos.WebApi/Mapper/EntityDTOProfile.cs
@@ -10,24 +10,11 @@ namespace FunBooksAndVideos.WebApi.Mapper
         {
             CreateMap<Entity.PurchaseOrder, PurchaseOrder>().ReverseMap();
             CreateMap<Entity.PurchaseOrderItem, PurchaseOrderItem>().ReverseMap();
-
-            //CreateMap<Entity.PurchaseOrder, PurchaseOrder>()
-            //    .ForMember(d => d.PurchaseOrderItems, s => s.MapFrom(m => m.PurchaseOrderItems));
-            //CreateMap<PurchaseOrder, Entity.PurchaseOrder>()
-            //    .ForMember(d => d.PurchaseOrderItems, s => s.MapFrom(m => m.PurchaseOrderItems));
-
             CreateMap<Entity.Customer, Customer>().ReverseMap();
             CreateMap<Entity.ShippingSlip, PurchaseOrder>().ReverseMap();
             CreateMap<Entity.Membership, Membership>().ReverseMap();
             CreateMap<Entity.ItemAttribute, ItemAttribute>().ReverseMap();
-
             CreateMap<Entity.Item, Item>().ReverseMap();
-            //CreateMap<Entity.Item, Item>()
-            //    .ForMember(d => d.Attributes, s => s.MapFrom(m => m.Attributes));
-            //CreateMap<Item, Entity.Item>()
-            //    .ForMember(d => d.Attributes, s => s.MapFrom(m => m.Attributes));
-
-
         }
     }
 }
